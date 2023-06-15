32412ba65f9925febf65984b005887b4e5efc2dc
kotobukid <kotobuki@daqua.jp>
2023-06-15 18:28:09 +0900

    不具合修正

--- ex/app.ts
+++ ex/app.ts
@@ -6,7 +6,7 @@ const app: express.Application = express();
 const router: Router = express.Router();
 router.get('/add/:a/:b', (req: Request<{ a: string, b: string }>, res: Response): void => {
     const _a: number = Number(req.params.a);
-    const _b: number = Number(req.params.a);
+    const _b: number = Number(req.params.b);
     res.send(`sum is ${add(_a, _b)}`);
 });
 
