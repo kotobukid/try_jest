b1395877d9d48bab037d3b91af042102f79b87a6
kotobukid <kotobuki@daqua.jp>
2023-06-15 16:13:51 +0900

    expressが動作する

--- ex/app.ts
+++ ex/app.ts
@@ -1,5 +1,21 @@
-import express from 'express';
+import express, {Request, Response, Router} from 'express';
 import {add} from "./funcs/math.js";
 
-console.log(express);
-console.log(add(100, 200));
+const app: express.Application = express();
+
+const port: number = 3000;
+
+const router: Router = express.Router();
+
+router.get('/add/:a/:b', (req: Request<{ a: string, b: string }>, res: Response): void => {
+    const _a: number = Number(req.params.a);
+    const _b: number = Number(req.params.a);
+    res.send(`sum is ${add(_a, _b)}`);
+});
+
+app.use(router);
+
+app.listen(port, (): void => {
+
+    console.log(`start listening on port ${port}`);
+});
